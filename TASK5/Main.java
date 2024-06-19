6bd50fa05ef0ced849beaae75ac57d575a7da83b
Divya-Dharshini-31 <sdivyadharshini315@gmaail.com>
2024-06-19 18:56:49 +0530

    Task5 classes created

---
+++ TASK5/Main.java
@@ -0,0 +1,9 @@
+package TASK5;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Main");
+        Student st=new Student();
+        st.print();
+    }
+}
---
+++ TASK5/Student.java
@@ -0,0 +1,7 @@
+package TASK5;
+
+public class Student {
+    public void print(){
+        System.out.println("Student class");
+    }
+}
